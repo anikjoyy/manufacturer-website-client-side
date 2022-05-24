d84c8f223b31a3b6de515d934e8ca39c5d20b22a
anikjoyy <anikjoy.cse@gmail.com>
2022-05-24 10:17:15 +0600

    created our location section and added it in home section

---
+++ src/Pages/Home/FindUs.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '450px',
+  height: '400px',
+};
+
+const center = {
+  lat: 24.37509992109359,
+  lng: 88.6046747626979,
+};
+
+const FindUs = () => {
+  return (
+    <div class='hero min-h-[60vh] bg-accent my-14'>
+      <div class='hero-content flex-col lg:flex-row'>
+        <div>
+          <h2 className='text-center text-5xl fw-bold text-primary'>
+            Our Location
+          </h2>
+          <p className='py-6 pr-7'>
+            If you want find our location just see the map right here!! And come
+            to us to build your dream car. If you don't want come here just
+            order the tools at your home.
+          </p>
+          <button class='btn btn-primary text-center'>Let's Go</button>
+        </div>
+        <div>
+          <LoadScript googleMapsApiKey='YOUR_API_KEY'>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+            >
+              <></>
+            </GoogleMap>
+          </LoadScript>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default React.memo(FindUs);
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import FindUs from './FindUs';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <FindUs></FindUs>
     </div>
   );
 };
