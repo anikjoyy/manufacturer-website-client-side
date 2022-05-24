6a13c6baa4fdf9399ac2ba92896f4ab07a681f67
anikjoyy <anikjoy.cse@gmail.com>
2022-05-24 10:29:42 +0600

    created contact section in home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import FindUs from './FindUs';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <FindUs></FindUs>
+      <Contact></Contact>
     </div>
   );
 };
