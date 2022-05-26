f69721bd83151214b647fef2f0227f719b8f3cdb
anikjoyy <anikjoy.cse@gmail.com>
2022-05-27 05:24:19 +0600

    showed product details from database using custom hooks

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Product = ({ product }) => {
-  const { id, name, img, minOrderQuantity, availableQuantity } = product;
+  const { _id, name, img, minOrderQuantity, availableQuantity } = product;
   const navigate = useNavigate();
 
   const navigateToDetails = (id) => {
@@ -25,7 +25,7 @@ const Product = ({ product }) => {
 
         <div className='card-actions '>
           <button
-            onClick={() => navigateToDetails(id)}
+            onClick={() => navigateToDetails(_id)}
             className='btn btn-primary rounded-xl hover:bg-yellow-200'
           >
             Buy Now
--- src/Pages/Home/ProductDetails.js
+++ src/Pages/Home/ProductDetails.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useProductDetails from '../../hooks/useProductDetails';
 
 const ProductDetails = () => {
+  const { id } = useParams();
+  const [product] = useProductDetails(id);
+
   return (
-    <div>
-      <h2>I am details</h2>
+    <div class='flex flex-col w-full lg:flex-row'>
+      <div class='card lg:card-side bg-base-100 shadow-xl my-5 ms-5'>
+        <figure>
+          <img style={{ height: '450px' }} src={product.img} alt='Album' />
+        </figure>
+        <div class='card-body'>
+          <h2 class='card-title'>Name: {product.name}</h2>
+          <div className='card-body'>
+            <p>Description: {product.shortDescription}</p>
+            <p>Minimum Order Quantity: {product.minOrderQuantity}</p>
+            <p>Available Quantity: {product.availableQuantity}</p>
+            <p>Price: {product.price}$ per unit</p>
+            <p>Supplier Name: {product.supplierName}</p>
+          </div>
+        </div>
+      </div>
+      <div class='divider lg:divider-horizontal'>OR</div>
+      <div class='grid flex-grow h-32 card bg-base-300 rounded-box place-items-center'>
+        content
+      </div>
     </div>
   );
 };
