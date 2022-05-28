28b690ea53c1d6bfdd9c7409ab3b4151245be28f
anikjoyy <anikjoy.cse@gmail.com>
2022-05-28 08:22:30 +0600

    created my profile section

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -21,7 +21,7 @@ const Dashboard = () => {
       </div>
       <div className='drawer-side'>
         <label htmlFor='dashboard-sidebar' className='drawer-overlay'></label>
-        <ul className='menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content'>
+        <ul className='menu p-4 overflow-y-auto w-48 bg-base-200 text-base-content'>
           {/* <!-- Sidebar content here --> */}
           {!admin && (
             <li>
--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/firebase.init';
 
 const MyProfile = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
-    <div>
-      <h2>my profile</h2>
+    <div class='hero min-h-[70vh]'>
+      <div class='card  w-full max-w-sm shadow-2xl bg-base-100'>
+        <div class='card-body'>
+          <div class='form-control'>
+            <label class='label'>
+              <span class='label-text'>Name</span>
+            </label>
+            <input
+              type='text'
+              disabled
+              value={user?.displayName || ''}
+              class='input input-bordered'
+            />
+          </div>
+          <div class='form-control'>
+            <label class='label'>
+              <span class='label-text'>Email</span>
+            </label>
+            <input
+              type='text'
+              disabled
+              value={user?.email || ''}
+              class='input input-bordered'
+            />
+          </div>
+          <div class='form-control'>
+            <label class='label'>
+              <span class='label-text'>Education</span>
+            </label>
+            <input
+              type='text'
+              placeholder='education'
+              class='input input-bordered'
+            />
+          </div>
+          <div class='form-control'>
+            <label class='label'>
+              <span class='label-text'>District</span>
+            </label>
+            <input
+              type='text'
+              placeholder='district'
+              class='input input-bordered'
+            />
+          </div>
+          <div class='form-control'>
+            <label class='label'>
+              <span class='label-text'>Phone Number</span>
+            </label>
+            <input
+              type='text'
+              placeholder='phone number'
+              class='input input-bordered'
+            />
+          </div>
+          <div class='form-control mt-6'>
+            <button class='btn btn-primary'>Update</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
