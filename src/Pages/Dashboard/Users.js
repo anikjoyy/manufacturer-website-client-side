d79640de3ee20f5a1c3535222c147ea722989307
anikjoyy <anikjoy.cse@gmail.com>
2022-05-28 06:23:58 +0600

    heroku

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -5,7 +5,7 @@ const Users = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/user', {
+    fetch('https://arcane-sierra-72100.herokuapp.com/user', {
       method: 'GET',
       headers: {
         authorization: `Bearer ${localStorage.getItem('accessToken')}`,
